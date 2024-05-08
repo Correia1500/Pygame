d6dbc8e08eca25896c662c2a6a560e54e4c4a4e5
Correia1500 <156720020+Correia1500@users.noreply.github.com>
2024-05-08 14:36:38 -0300

    logica

---
+++ src/game.py
@@ -0,0 +1,103 @@
+import pygame
+
+pygame.init()
+
+# Gera tela principal
+
+WIDTH = 800
+HEIGHT = 600
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pygame")
+
+
+
+# Inicia assets
+
+plataform_img = pygame.image.load("assets/plataforma.png")
+plataform_img = pygame.transform.scale(plataform_img, (WIDTH, 100))
+player_img = pygame.image.load("assets/quadrado.png")
+player_img = pygame.transform.scale(player_img, (50, 50))
+
+## ----- Inicia estruturas de dados
+# Definindo os novos tipos
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self,img):
+        # Construtor da classe mãe
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH/2
+        self.rect.bottom = HEIGHT - 100
+        self.speedx = 0
+    
+    def update(self):
+        # Atualiza a posição do jogador
+        self.rect.x += self.speedx
+
+        # Mantem dentro da tela
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0: 
+            self.rect.left = 0
+
+
+game = True
+
+# variavel para ajustar a velocidade do jogo
+clock = pygame.time.Clock()
+FPS = 30
+
+#cria um grupo de sprites e adiciona o jogador
+all_sprites = pygame.sprite.Group()
+
+player = Player(player_img)
+all_sprites.add(player)
+
+# for i in range(10):
+#     #player = Player(player)
+#     all_sprites.add(player)
+
+#Loop principal
+
+while game:
+    clock.tick(FPS)
+
+    # Eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+
+        #Verifica se apertou alguma tecla
+        if event.type ==pygame.KEYDOWN:
+            #Dependendo da tecla, altera a velocidade
+            if event.key == pygame.K_LEFT:
+                player.speedx -= 8
+            if event.key == pygame.K_RIGHT:
+                player.speedx += 8
+        # verifica se soltou alguma tecla
+        if event.type == pygame.KEYUP:
+            # Dependendo da tecla, altera a velocidade
+            if event.key == pygame.K_LEFT:
+                player.speedx += 8
+            if event.key == pygame.K_RIGHT:
+                player.speedx -= 8 
+
+    #atualiza estado do jogo
+         
+    #atualiza a posição do jogador
+    all_sprites.update()
+
+
+    #gera saidas
+    window.fill((255, 255, 255)) #Preenche a tela com a cor branca
+    window.blit(plataform_img, (0, 500))
+    all_sprites.draw(window)  #Desenha o jogador na tela
+
+    #atualiza a tela
+    pygame.display.update()
+
+#Finaliza o pygame
+pygame.quit()
